4ff359d3850117bfcd3bba3233c8ead300a6cb07
NotAGreatsername <107520328+NotAGreatsername@users.noreply.github.com>
2022-12-22 09:35:43 -0500

    Add files via upload

---
+++ UpgradesFrame.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+class UpgradesFrame{
+  
+  String displayScreen;
+  int x, y, width, height;
+  ArrayList<Upgrade> upgrades;
+  
+  UpgradesFrame(String displayScreen, int x, int y, int width, int height, ArrayList<Upgrade> upgrades){
+    this.displayScreen = displayScreen;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.upgrades = upgrades;
+  }
+  
+  void sort(String sortOrder){
+    
+  }
+  
+  void draw(Graphics g){
+    
+  }
+}
