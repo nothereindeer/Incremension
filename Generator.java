2308d66ea284228417fd7e35dd3eb5678717da88
dilhrm <121042602+dilhrm@users.noreply.github.com>
2022-12-22 09:35:38 -0500

    Add files via upload

---
+++ Generator.java
@@ -0,0 +1,15 @@
+
+class Generator{
+    
+  String name; 
+  Currency currencyMade; 
+  String production; 
+  
+  Generator(String name, Currency currencyMade, String baseProduction){
+    this.name = name; 
+    this.currencyMade = currencyMade; 
+    this.production = baseProduction;
+  }  
+  
+  //void addProduction(String amount)  
+}
