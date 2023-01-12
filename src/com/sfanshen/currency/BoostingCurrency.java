e185c4304359789cfda739f80f6c0610f66779ea
NotAGreatsername <rx4pxr@gmail.com>
2023-01-12 16:18:01 -0500

    second commit

--- src/com/sfanshen/currency/BoostingCurrency.java
+++ src/com/sfanshen/currency/BoostingCurrency.java
@@ -4,8 +4,8 @@ import com.sfanshen.graphics.Picture;
 import com.sfanshen.main.Formula;
 
 public class BoostingCurrency extends Currency{
-  Formula formula;
-  Currency boostedCurrency;
+  private Formula formula;
+  private Currency boostedCurrency;
   
   public BoostingCurrency(String name, Picture icon, Currency boostedCurrency, Formula formula){
     super(name, icon);
